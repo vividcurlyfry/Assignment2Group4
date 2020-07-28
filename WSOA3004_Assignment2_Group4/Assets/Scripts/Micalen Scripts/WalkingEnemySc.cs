a4c1e6cb023a26580313df0aebd418d50b706bcd
Micalen <64082857+Micalen@users.noreply.github.com>
2020-07-28 23:16:16 +0200

    Day 2 Update 3
    made some platforms and enemy

---
+++ WSOA3004_Assignment2_Group4/Assets/Scripts/Micalen Scripts/WalkingEnemySc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingEnemySc : MonoBehaviour
+{
+    public static WalkingEnemySc instance;
+    public Rigidbody2D ThisEnemyRB;
+    public float WalkSpeed;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+      ThisEnemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThisEnemyRB.velocity = new Vector2(WalkSpeed, ThisEnemyRB.velocity.y);
+    }
+}
