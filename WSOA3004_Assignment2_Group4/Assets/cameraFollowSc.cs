6b328bce00a94ad8e9ce0122eb38b34000ac0ed4
Micalen <64082857+Micalen@users.noreply.github.com>
2020-07-28 22:02:15 +0200

    Day 2 Update 3
    made the camera follow

---
+++ WSOA3004_Assignment2_Group4/Assets/cameraFollowSc.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowSc : MonoBehaviour
+{
+    public GameObject PlayerFollow;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 pos = this.transform.position;
+        pos.y = Mathf.Lerp(this.transform.position.y, PlayerFollow.transform.position.y, interpolation);
+        pos.x = Mathf.Lerp(this.transform.position.x, PlayerFollow.transform.position.x, interpolation);
+
+        this.transform.position = pos;
+    }
+}
