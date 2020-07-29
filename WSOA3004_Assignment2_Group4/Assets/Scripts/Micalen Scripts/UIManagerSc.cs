6952aac76241a8328a92f24a0f9452d038f490ab
Micalen <64082857+Micalen@users.noreply.github.com>
2020-07-29 23:05:53 +0200

    UI Improvement

--- WSOA3004_Assignment2_Group4/Assets/Scripts/Micalen Scripts/UIManagerSc.cs
+++ WSOA3004_Assignment2_Group4/Assets/Scripts/Micalen Scripts/UIManagerSc.cs
@@ -8,6 +8,7 @@ public class UIManagerSc : MonoBehaviour
     public static UIManagerSc instance;
     public Text ToeTxt, BrainTxt, HeartTxt;
     public int Toes = 0, Brains = 0, Hearts = 0;
+    public int TotalToes, TotalBrains, TotalHearts;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -21,8 +22,8 @@ public class UIManagerSc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ToeTxt.text = Toes.ToString() + "/3";
-        BrainTxt.text = Brains.ToString() + "/2";
-        HeartTxt.text = Hearts.ToString() + "/1";
+        ToeTxt.text = Toes.ToString() + "/" + TotalToes.ToString();
+        BrainTxt.text = Brains.ToString() + "/" + TotalBrains.ToString();
+        HeartTxt.text = Hearts.ToString() + "/" + TotalHearts.ToString();
     }
 }
