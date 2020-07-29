97bf971772eb494ac757891398f0ee9bdbaf07c4
Micalen <64082857+Micalen@users.noreply.github.com>
2020-07-29 22:53:59 +0200

    UI and Out-of-bounds
    I coded the UI text to change when an item is picked up and now if you fall off the platform you die!

---
+++ WSOA3004_Assignment2_Group4/Assets/UIManagerSc.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerSc : MonoBehaviour
+{
+    public static UIManagerSc instance;
+    public Text ToeTxt, BrainTxt, HeartTxt;
+    public int Toes = 0, Brains = 0, Hearts = 0;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToeTxt.text = Toes.ToString() + "/3";
+        BrainTxt.text = Brains.ToString() + "/2";
+        HeartTxt.text = Hearts.ToString() + "/1";
+    }
+}
